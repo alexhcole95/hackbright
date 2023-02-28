3b2ec7cae783276cf2c8b4f180999f29d4e93fb6
Alex Cole <alexhcole95@gmail.com>
2023-02-28 11:45:26 -0600

    Edited and relocated file

---
+++ Week 4/Day 2/Lab/Part 1/mapit.py
@@ -0,0 +1,17 @@
+#!/Library/Frameworks/Python.framework/Versions/3.11/bin/python3
+
+
+"""Launch a map in the browser using an address from the command line
+or clipboard """
+import webbrowser
+import sys
+import pyperclip
+
+if len(sys.argv) > 1:
+    # Get address from command line
+    address = " ".join(sys.argv[1:])
+else:
+    # Get address from clipboard
+    address = pyperclip.paste()
+
+webbrowser.open(f"https://www.google.com/maps/place/{address}")
--- Week 4/Day 2/Lab/mapit.py
+++
@@ -1,9 +0,0 @@
-# Launch a map in the browser using an address from the command line
-# or clipboard,
-import webbrowser
-import sys
-
-if len(sys.argv) > 1:
-    # Get address from command line.
-    address = " ".join(sys.argv[1:])
-
